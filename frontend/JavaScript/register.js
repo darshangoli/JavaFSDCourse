2449433eeebaf5cf87b9c44f5ac6792366f1e2d3
darshangoli <satyadarshan4022@gmail.com>
2026-03-01 18:22:37 +0530

    feat: Add Loops.html and Switch.html for number processing examples; enhance register.js with additional validation for subjects and location

--- frontend/JavaScript/register.js
+++ frontend/JavaScript/register.js
@@ -5,14 +5,14 @@ function validateForm(){
         return false;
     }
     console.log(fname);
-
+    
     let secname = document.getElementById("lname").value;
     if(secname == ""){
         alert("please fill the Second Name");
         return false;
     }
     console.log(secname);
-
+// 91012a8876
     let phone = document.getElementById("phone").value;
     let pattern = /^[0-9]+$/;
     if(phone == "" || !phone.match(pattern)){
@@ -22,12 +22,32 @@ function validateForm(){
     console.log(phone);
 
     let age = document.getElementById('age').value;
-    if(age < 0 || !age.match(pattern)){
+    if(age < 1 || age == ""){
         alert("Enter the correct age");
         return false;
     }
     console.log(age);
+
+    let subjects = document.getElementsByName("subjects");
+    let selectedSubject = false;
+    for(let i=0; i<subjects.length; i++){
+        if(subjects[i].checked){
+            selectedSubject = true;
+            break;
+        }
+    }
+    if(selectedSubject == false){
+        alert("select atleast one subject");
+        return false;
+    }
+
+    let location = document.getElementById("location").value;
+    if(location == ""){
+        alert("Enter the location");
+        return false;
+    }
+    console.log(location);
+
+    alert("form submitted succesfully")
     return false;
-
-
 }
